ba8f0354b9db65b98919fcb6d2ea50845488fc8b
Blake <pelleyb28@cbchs.org>
2024-11-25 08:22:18 -0600

    xxx

--- classes_pelley.py
+++ classes_pelley.py
@@ -6,7 +6,7 @@ class User:
         self.last_name = "Pelley"
         self.friend_last_name = "Province"
         self.friend_first_name = "Gunner"
-
+#a
         
     def describe_user(self):
         print(f"Your name is {self.first_name} {self.last_name}, and your friend's name is {self.friend_first_name} {self.friend_last_name}.")
